32e1e6746ab934e1d5ea0661c38cb71460b1daec
SeongHo5 <newfuring1123@gmail.com>
2023-06-10 14:33:06 +0900

    reponse 테스트

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import * as mysql from "./lib/mysql";
 
 const app = express();
 const port = 5000;
 
 app.listen(port, () => {
-    console.log("success connection!!");
+    console.log(`Server is listening on port ${port}`);
     mysql.connect();
+});
+
+app.get('/request', (req: Request, res: Response) => {
+    res.send(`Server is running`);
 });
